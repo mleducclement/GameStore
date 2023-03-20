3da01ea7cc797d92b619b912aa2d3ba888e7cc34
Michael Leduc <michael.leducclement@gmail.com>
2023-03-19 20:04:47 -0400

    Add DbContext and connstring

---
+++ GameStoreWeb/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using GameStoreWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameStoreWeb.Data {
+    public class AppDbContext : DbContext {
+        DbSet<User> Users { get; set; }
+        DbSet<Genre> Genres { get; set; }
+        DbSet<Product> Products { get; set; }
+        DbSet<Order> Orders { get; set; }
+        DbSet<OrderProduct> OrderProducts { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {
+
+        }
+    }
+}
---
+++ GameStoreWeb/Models/OrderProduct.cs
@@ -0,0 +1,8 @@
+﻿namespace GameStoreWeb.Models {
+    public class OrderProduct {
+        public int Id { get; set; }
+        public required Product Product { get; set; }
+        public required Order Order { get; set; }
+        public int Quantity { get; set; }
+    }
+}
